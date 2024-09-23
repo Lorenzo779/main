1d12e23e3d7eba434eb1ede78594d3cd7d32cd6c
Lo.merola <82643528+Lorenzo779@users.noreply.github.com>
2024-09-23 17:00:16 +0200

    Add files via upload

---
+++ java/src/com/prove/lambda/basics/FunctionalProgrammingApp.java
@@ -0,0 +1,11 @@
+package com.prove.lambda.basics;
+
+public class FunctionalProgrammingApp {
+
+	public static void main(String[] args) {
+		WelcomeMessageService welcomeMessage = new WelcomeMessageService();
+		welcomeMessage.greet();
+
+	}
+
+}
